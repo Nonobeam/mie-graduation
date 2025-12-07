adad6cae1cdaf1b844a9047cd236790b9c6c338f
Nonobeam <nguyenhuuthuc0405@gmail.com>
2025-12-07 06:44:19 +0000

    Change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: false, // Bundle all CSS into one file
+    assetsInlineLimit: 0, // Don't inline assets
+  },
   server: {
     proxy: {
       '/api/sheets': {
